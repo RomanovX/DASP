d4bf9135ac1c792574c6927aa3b82a511a6b6ed4
Remy <remykabel.tu@gmail.com>
2015-06-13 15:55:50 +0200

    Added loading of wavs (and ignore m~ files)

--- Pnn.m
+++ Pnn.m
@@ -9,4 +9,13 @@ clear all
 clc
 close all
 
-%%
+%% 1: Loading audio & combining clean + noise
+
+Fs = 16000;                                                     % Sampling frequency of 16000 Hz (same for all)   
+
+[clean] = audioread('/audio/clean.wav');                        % Clean signal
+[noise1] = audioread('/audio/noise1.wav');                      % Noise 1
+[noise2] = audioread('/audio/intersection_soundjay.wav');       % Noise 2
+
+noisy1 = clean + noise1;
+%noisy2 = clean + noise2;
