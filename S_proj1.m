a162e103862eeb960ee428d4c3bd884d991c92fd
Yads-gh7076 <yadnyesh.luktuke@gmail.com>
2015-06-18 14:45:40 +0200

    Zero padding ip and 512 samples

--- S_proj1.m
+++ S_proj1.m
@@ -2,12 +2,32 @@ clc;
 clear;
 [s,fs]=wavread('F:\TU Delft courses\Speech and Audio Processing\clean.wav');
 n1=wavread('F:\TU Delft courses\Speech and Audio Processing\noise1.wav');
-spf=400;
+spf=512;
 y1=s+n1;
-[m,n]=size(y1);
-f=round(m/spf);
-y=zeros(512,f);
-for i=1:f
-    y(1:spf,i)=y1(spf*(i-1)+1:spf*i,1);
+% Appending the speech signal with zeros
+m=length(y1);
+m1=m;
+while(rem(m1,spf)~=0)
+    m1=m1+1;
 end
-y(spf+1:spf+m-(spf*f),f)=y1((f*spf)+1:m,1);
+a=zeros(m1,1);
+a(1:m,1)=y1;
+% Segmentation and computing the 512 point DFT
+f=m1/spf;
+y=zeros(spf,f);
+for i=1:f
+    y(1:spf,i)=a(spf*(i-1)+1:spf*i,1);
+end
+Y=fft(y);
+% Computing the periodogram of the individual segments
+Py=(abs(Y).*abs(Y))/(spf);
+% Computing the Bartlett estimate of the signal
+PyB=sum(Py,2);
+PyB=PyB/f;
+plot(PyB)
+
+
+        
+
+
+        
